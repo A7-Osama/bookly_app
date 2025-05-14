81b291bb59d822dcbad5dc601aa961ee310b85c1
A7-Osama <ao699168@gmail.com>
2025-05-15 02:53:29 +0300

    adding Horizontal ListView Builder

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
+      padding: EdgeInsets.only(left: 24, right: 24, top: 40, bottom: 30),
       child: Row(
         children: [
           Image.asset(AssetsData.logo, height: 22),
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/h_list_view_item.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +11,29 @@ class HomeViewBody extends StatelessWidget {
       children: [
         CustomAppBar(),
         //CustomBody(),
-        HListViewItem(),
+        // HListViewItem(),
+        HListViewBuilder(),
       ],
+    );
+  }
+}
+
+class HListViewBuilder extends StatelessWidget {
+  const HListViewBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: HListViewItem(),
+          );
+        },
+      ),
     );
   }
 }
