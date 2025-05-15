3873c32c4898ad845c4f35e2209256b2a93c5c0e
A7-Osama <ao699168@gmail.com>
2025-05-15 07:44:08 +0300

    adding Vertical ListView Item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/h_list_view_builder.dart';
@@ -17,6 +18,51 @@ class HomeViewBody extends StatelessWidget {
           HListViewBuilder(),
           SizedBox(height: 50),
           Text('Best Sellers', style: Styles.titleMedium),
+          VListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class VListViewItem extends StatelessWidget {
+  const VListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                color: Colors.red,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              children: [
+                // Text('Title'),
+                // Text('Title'),
+                /* Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Title'),
+                    RatingWidget(),
+                  ], 
+                ),*/
+              ],
+            ),
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
-// import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -18,9 +17,7 @@ class BooklyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratAlternatesTextTheme(
-          ThemeData.dark().textTheme,
-        ),
+        textTheme: GoogleFonts.robotoTextTheme(ThemeData.dark().textTheme),
       ),
       home: const SplashView(),
     );
