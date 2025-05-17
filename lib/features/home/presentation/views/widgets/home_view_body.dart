aeb978e55d564a3544c4c09c1780bb8d317b196a
A7-Osama <ao699168@gmail.com>
2025-05-17 07:02:07 +0300

    Vertical List View Item - One More Touch To Finish It

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
-import 'package:bookly_app/core/utils/assets_data.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/h_list_view_builder.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/v_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -10,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 24.0),
+      padding: const EdgeInsets.only(left: 30.0),
       child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -18,51 +18,8 @@ class HomeViewBody extends StatelessWidget {
           HListViewBuilder(),
           SizedBox(height: 50),
           Text('Best Sellers', style: Styles.textStyle18),
+          SizedBox(height: 20),
           VListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class VListViewItem extends StatelessWidget {
-  const VListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(8)),
-                color: Colors.red,
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(AssetsData.testImage),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              children: [
-                Text('Title'),
-                Text('Title'),
-                /* Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Title'),
-                    RatingWidget(),
-                  ], 
-                ),*/
-              ],
-            ),
-          ),
         ],
       ),
     );
---
+++ lib/features/home/presentation/views/widgets/v_list_view_item.dart
@@ -0,0 +1,63 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class VListViewItem extends StatelessWidget {
+  const VListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                color: Colors.red,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 30),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .55,
+                child: Text(
+                  'Harry Potter And The Goblet of Fire',
+                  // textAlign: TextAlign.center,
+                  style: Styles.textStyle20.copyWith(fontFamily: kGTSectraFine),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              const SizedBox(height: 3),
+              const Text('J.K.  Rowling', style: Styles.textStyle14),
+              const SizedBox(height: 3),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '19.99 €',
+                    style: Styles.textStyle20.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  // RatingWidget(),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
